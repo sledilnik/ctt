4e1dfa4566a1842721a43383e0d9d3d4e21355cf
Stefan Baebler <stefan.baebler@gmail.com>
2020-10-03 12:25:49 +0200

    do not set locale for now

--- generate_html.py
+++ generate_html.py
@@ -1,14 +1,12 @@
 from jinja2 import Template
 from pathlib import Path
 from datetime import datetime
-import locale
 
 available_dates = Path("page/keys/").iterdir()
 available_dates = sorted([val.stem for val in available_dates if not val.name == ".gitkeep"], reverse=True)
 print(available_dates)
 
-locale.setlocale(locale.LC_ALL, 'sl_SI.UTF-8')
-now = datetime.now().strftime("%A, %_d. %B %Y ob %H:%M")
+now = datetime.now().strftime("%_d. %_m. %Y ob %H:%M")
 print(now)
 
 with open("page/template.html") as f:
