64a163be289caddbd945a947ff9d19d91409ecbf
Stefan Baebler <stefan.baebler@gmail.com>
2020-10-16 08:30:45 +0200

    re-enable filehashes.json generation and publication

--- cleanup_before_publication.sh
+++ cleanup_before_publication.sh
@@ -3,6 +3,6 @@ rm -rf page/keys_hourly
 rm -rf page/json_hourly
 rm -rf page/users_hourly
 rm -rf page/keys
-rm -rf page/json
+rm -rf page/json/????-??-??.json
 rm -rf page/users
 rm -rf page/plaintext
